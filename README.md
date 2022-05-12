6aba3afda6bac9d2634dc4e0989a1538a55f848e
Adam <a.jaskierski@tkhtechnology.com>
2022-05-12 09:54:54 +0200

    "rename,dockerfile"

--- README.md
+++ README.md
@@ -1,11 +1,40 @@
-# api-excercise
+#  api-excercise
+
+
+
 Przykładowe api, korzystające z mongodb
 
-***Dane potrzebne do połączenia się należy uzupełnić w pliku ".env"***
+***Dane potrzebne do połączenia się należy uzupełnić w pliku *.env****
 
-Posiada 5 prostych endpointów:
-- create_item
-- delete_item
-- get_item *(przy użyciu indexu)*
-- get_item *(przy użyciu zawratości)*
-- update_item
+Posiada 8 prostych endpointów:
+- get/user
+- post/user
+- post/item
+- delete/item
+- get/itemIndex *(przy użyciu indexu)*
+- get/itemMessage *(przy użyciu zawratości)*
+- put/itemID
+
+**pipenv**
+
+Za pomocą Pipfile/Pipfile.lock i PipEnv z łatwością można
+zainstalować wszystkie zależności projektu
+
+<code>pipenv install --version </code> - aby sprawdzić wersję/czy jest zainstalowany
+
+<code>pip install pipenv</code> - Instalacja *pipenv* za pomocą *pip*
+
+**Uruchamianie**
+
+*Przed rozpoczeciem uruchamiania uzupełniamy dane do połączenia się z MongoDB w pliku .env*
+
+<code>pipenv install</code> - instalujemy wszystkie moduły
+
+<code>uvicorn api:app</code> - uruchamiamy webserver
+
+Gotowe, teraz api działa na socketcie 127.0.0.1:8000
+
+Można go używać przy pomocy *curl* lub przeglądarki
+
+<a href="https://www.python.org" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg" alt="python" width="40" height="40"/> </a>
+
--- api.py
+++ api.py
@@ -1,26 +1,62 @@
-from email.headerregistry import Address
 import time
+import secrets
 import os
 from typing import Optional
 import pymongo
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
 from pydantic import BaseModel
 from dotenv import load_dotenv
 
+description = """
+Przykładowe api, korzystające z mongodb
+
+***Dane potrzebne do połączenia się należy uzupełnić w pliku <i>.env</i>***
+"""
+
+
+tags_metadata = [
+    {
+        "name":"root",
+        "description":"Shows all data after authentication"
+    },
+    {
+        "name":"users",
+        "description" : "user management"
+    },
+    {
+        "name":"items",
+        "description":"items management"
+    }
+
+]
+
 load_dotenv()
 
-username = os.environ("USERNAME_MONGODB")
-password = os.environ("PASSWORD")
-cluster_address = os.environ("CLUSTER_ADDRESS")
+username = os.getenv("USERNAME_MONGODB")
+password = os.getenv("PASSWORD")
+cluster_address = os.getenv("CLUSTER_ADDRESS")
 
 # connection
 client = pymongo.MongoClient(
     f"mongodb+srv://{username}:{password}@{cluster_address}.fmeh0.mongodb.net/{cluster_address}?retryWrites=true&w=majority")
 db = client["api_db"]
 collection = db['db']
+collection_users = db["db_users"]
 
 # API part
-app = FastAPI()
+app = FastAPI(
+    openapi_tags=tags_metadata,
+    description=description,
+    title="api-excercise",
+    version="1.0.1",
+    contact={
+        "name":"Adam Jaskierski",
+        "email": "a.jaskierski@tkhtechnology.com"
+    }
+    )
+
+security = HTTPBasic()
 
 
 class Item(BaseModel):
@@ -31,13 +67,45 @@ class Item(BaseModel):
 class UpdateItem(BaseModel):
     message: Optional[str] = None
 
-@app.get("/")
-async def root():
+
+class User(BaseModel):
+    username: str
+    password: str
+
+
+# test using curl -u username:passwd /// browser remembers credentials
+# has pass and store in db
+def get_current_username(credentials: HTTPBasicCredentials = Depends(security)):
+    correct_username = secrets.compare_digest(credentials.username, "test1")
+    correct_password = secrets.compare_digest(credentials.password, "pass1")
+    if not (correct_username and correct_password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect email or password",
+            headers={"WWW-Authenticate": "Basic"},
+        )
+    return credentials.username
+
+
+@app.get("/", tags=["root"])
+async def root(current_user: str = Depends(get_current_username)):
     # shows all of data
     all_data = list(collection.find({}))
     return all_data
 
-@app.post("/create_item/")
+
+@app.get("/gets/user",tags=["users"])
+def read_current_user(username: str = Depends(get_current_username)):
+    return {"username": username}
+
+
+@app.post("/posts/users",tags=["users"])
+def create_user(user: User):
+    collection_users.insert_one(user.__dict__)
+    return {"Status": "Success"}
+
+
+@app.post("/posts/item",tags=["items"])
 def create_item(item: Item):
     # extra info about new message plus _id
     item_dict = item.__dict__
@@ -60,14 +128,14 @@ def create_item(item: Item):
 
 
 # delete
-@app.delete("/delete/{index}")
+@app.delete("/deletes/itemIndex/{index}",tags=["items"])
 def delete_item(index: int):
     collection.delete_one({"_id": index})
     return {"Status": "Success"}
 
 
 # get by id
-@app.get("/get_item/{index}")
+@app.get("/gets/itemIndex/{index}",tags=["items"])
 def get_item(index: int):
     data = collection.find({"_id": index})
     dic = {}
@@ -75,12 +143,12 @@ def get_item(index: int):
         dic[n] = i
     if dic:
         return dic
-    else: 
-        return {"Status" : "0 results found"}
+    else:
+        return {"Status": "0 results found"}
 
 
 # get by message
-@app.get("/get_item/{message}")
+@app.get("/gets/itemMessage/{message}",tags=["items"])
 def get_item(message: str):
     data = collection.find({"message": message})
     dic = {}
@@ -88,11 +156,11 @@ def get_item(message: str):
         dic[n] = i
     if dic:
         return dic
-    else: 
-        return {"Status" : "0 results found"}
+    else:
+        return {"Status": "0 results found"}
 
 
-@app.put("/update_item/{item_id}")
+@app.put("/puts/itemId/{item_id}",tags=["items"])
 def update_item(item_id: int, item: UpdateItem):
     match = collection.find_one({"_id": item_id})
     if not match:
